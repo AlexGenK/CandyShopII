4894fe4a74b7612fee280e621792437c213dce58
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-05-12 15:34:00 +0300

    add visit view

--- app.rb
+++ app.rb
@@ -3,28 +3,31 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+# обявление БД
 set :database, "sqlite3:candyshop.db"
 
+# Сущность/таблица - клиент
 class Client < ActiveRecord::Base
 end
 
+# Сущность/таблица - конфетка
 class Candy < ActiveRecord::Base
 end
 
 # процедура инициализации
 configure do
-
+	# список конфеток
+	$candies=Candy.order "candyname"
 end
 
 # главная страница
 get '/' do
-	@candies=Candy.order "candyname"
 	erb :index
 end
 
-# 1
-get '/one' do
-	erb "<h2> One </h2>"
+# запись
+get '/visit' do
+	erb :visit
 end
 
 # 2
@@ -36,6 +39,21 @@ end
 get '/three' do
 	erb "<h2> Three </h2>"
 end
+
+# обработка записи
+post '/visit' do
+
+	# переменные содержащие значения полей ввода
+	@username=params[:username]
+	@phone=params[:phone]
+	@data=params[:data]
+	@candy=params[:candy]
+	@colorpicker=params[:colorpicker]
+
+	erb :visit
+
+end
+
 
 # возвращает сообщение о возможных ошибках. принмимает хеш с парой
 # имя_параметра=>выводимое сообщение. если параметр пустой, формируется сообщение
